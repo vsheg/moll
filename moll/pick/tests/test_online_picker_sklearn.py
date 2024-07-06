de8bc5fb206cbba9c66e728400dbb6a1c2ee8d44
Vsevolod Shegolev <36817377+vsheg@users.noreply.github.com>
2024-07-06 18:00:09 +0300

    test: remove picker fixture

--- moll/pick/tests/test_online_picker_sklearn.py
+++ moll/pick/tests/test_online_picker_sklearn.py
@@ -10,11 +10,6 @@ from moll.pick._online_picker import OnlineVectorPicker
 
 
 @pytest.fixture
-def picker():
-    return OnlineVectorPicker(capacity=10)
-
-
-@pytest.fixture
 def X():
     return np.random.rand(10, 3)
 
@@ -24,17 +19,12 @@ def y():
     return np.arange(10)
 
 
-def test_pipeline(picker, X, y):
-    # Fit the picker directly
-    picker_copied = deepcopy(picker)
-    picker_copied.fit(X, y)
-    X_transformed_picker_copied = picker_copied.transform(X)
-
+def test_pipeline(X, y):
     # Fit the picker through a pipeline
     pipeline = Pipeline(
         [
             ("scaler", StandardScaler()),
-            ("picker", picker),
+            ("picker", OnlineVectorPicker(capacity=10)),
             # to test it can handle extra steps, nothing to impute here
             ("imp", SimpleImputer(strategy="mean")),
         ]
@@ -42,12 +32,15 @@ def test_pipeline(picker, X, y):
     pipeline.fit(X, y)
     X_transformed_pipeline = pipeline.transform(X)
 
+    # Fit the picker directly
+    picker2 = OnlineVectorPicker(capacity=10)
+    picker2.fit(X, y)
+    X_transformed2 = picker2.transform(X)
+
     # Labels are the same
-    assert picker_copied.labels == pipeline.named_steps["picker"].labels
+    assert picker2.labels == pipeline.named_steps["picker"].labels
 
     # Vectors are the same after scaling
-    X_transformed_picker_copied_scaled = (
-        X_transformed_picker_copied - np.mean(X, axis=0)
-    ) / np.std(X, axis=0)
+    X_transformed2_scaled = (X_transformed2 - np.mean(X, axis=0)) / np.std(X, axis=0)
 
-    assert np.allclose(X_transformed_pipeline, X_transformed_picker_copied_scaled)
+    assert np.allclose(X_transformed_pipeline, X_transformed2_scaled)
