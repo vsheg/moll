31c455b9a3dac938f7cc22d34a4bbeb1a027e863
Vsevolod Shegolev <v@sheg.cc>
2024-07-16 18:59:40 +0300

    feat: Add support for maximizing loss in `_add_vector`

--- moll/pick/tests/test_online_add.py
+++ moll/pick/tests/test_online_add.py
@@ -162,7 +162,36 @@ def test_add_vector_with_pinned(X, X_pinned, x, updated_idx):
         loss_fn=lambda s: s**-1,
         k_neighbors=5,
         n_valid_vectors=5,
-        min_sim=0.0,
+    )
+    assert upd_idx == updated_idx
+
+    if updated_idx == -1:
+        assert (X_copy == X_updated).all()
+    else:
+        assert (X_updated == X_copy.at[updated_idx].set(x)).all()
+
+
+@pytest.mark.parametrize(
+    "x, updated_idx",
+    [
+        ([1.1, 1.1], 4),
+        ([0.1, 0.1], 4),
+        ([4.1, 4.1], 0),
+    ],
+)
+def test_add_vector_with_maximize(X, x, updated_idx):
+    X_copy = X.copy()
+    x = jnp.array(x)
+
+    X_updated, upd_idx = _add_vector(
+        x=x,
+        X=X,
+        dist_fn=euclidean,
+        sim_fn=lambda d: d,
+        loss_fn=lambda s: s**-1,
+        k_neighbors=5,
+        n_valid_vectors=len(X),
+        maximize=True,
     )
     assert upd_idx == updated_idx
 
@@ -217,7 +246,8 @@ def test_update_vectors(X, X_pinned, xs, acc_mask):
         loss_fn=lambda s: s**-1,
         k_neighbors=5,
         n_valid=5,
-        min_sim=0.0,
+        sim_min=0.0,
+        sim_max=jnp.inf,
     )
 
     assert X_copy.shape == X_updated.shape
