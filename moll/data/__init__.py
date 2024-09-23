afcc1fbbd09ad813b9fe4f563c49847bf00b7865
Vsevolod Shegolev <v@sheg.cc>
2024-09-23 18:51:30 +0300

    feat: add data files module for testing and examples

---
+++ moll/data/__init__.py
@@ -0,0 +1,8 @@
+"""
+Data provided for testing, examples, documentation, etc.
+"""
+
+from pathlib import Path
+
+amino_acids_smi = Path(__file__).parent / "amino_acids.smi"
+d_aldopentoses_sdf: Path = Path(__file__).parent / "d_aldopentoses.sdf"
