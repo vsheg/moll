65f9e0c1a4786200b760f83eb64d771b8d2be87d
Vsevolod Shegolev <v@sheg.cc>
2024-09-26 18:06:41 +0300

    feat: add saturated fatty acids mol2 file

--- moll/data/__init__.py
+++ moll/data/__init__.py
@@ -12,3 +12,7 @@ amino_acids_smi: Path = (_parent / "amino_acids.smi").relative_to(_cwd, walk_up=
 d_aldopentoses_sdf: Path = (_parent / "d_aldopentoses.sdf").relative_to(
     _cwd, walk_up=True
 )
+
+saturated_fatty_acids_mol2 = (_parent / "saturated_fatty_acids.mol2").relative_to(
+    _cwd, walk_up=True
+)
