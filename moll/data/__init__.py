269f222b1dce47eb8279f58ad651395708f8ee7c
Vsevolod Shegolev <v@sheg.cc>
2024-09-26 18:04:31 +0300

    refactor: make file paths relative to user

--- moll/data/__init__.py
+++ moll/data/__init__.py
@@ -4,5 +4,11 @@ Data provided for testing, examples, documentation, etc.
 
 from pathlib import Path
 
-amino_acids_smi = Path(__file__).parent / "amino_acids.smi"
-d_aldopentoses_sdf: Path = Path(__file__).parent / "d_aldopentoses.sdf"
+_parent = Path(__file__).parent  # is moll/data
+_cwd = Path.cwd()  # user's working directory
+
+amino_acids_smi: Path = (_parent / "amino_acids.smi").relative_to(_cwd, walk_up=True)
+
+d_aldopentoses_sdf: Path = (_parent / "d_aldopentoses.sdf").relative_to(
+    _cwd, walk_up=True
+)
